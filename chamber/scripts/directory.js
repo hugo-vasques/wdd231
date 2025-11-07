6c16837551058be1053d417e959be5e9881ffdcb
MaCK34 <prom23hugomarcelo.sa@gmail.com>
2025-11-06 19:06:07 -0500

    created a JS to enlist every busniness, display them as grid or listed elements, style and a .json with every business for the page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,77 @@
+const directoryContainer = document.querySelector('#directoryContainer');
+const gridBtn = document.querySelector('#gridBtn');
+const listBtn = document.querySelector('#listBtn');
+
+let businesses = [];
+
+fetch('./data/business.json')
+    .then(response => response.json())
+    .then(data => {
+        businesses = data;
+        displayGrid();
+    });
+
+function displayGrid() {
+    directoryContainer.innerHTML = '';
+
+    businesses.forEach(biz => {
+        const card = document.createElement('section');
+        card.classList.add('card');
+
+        const membershipNames = {
+            1: "Bronze",
+            2: "Silver",
+            3: "Gold"
+        };
+
+        const membershipText = membershipNames[biz.membership];
+
+        card.classList.add(membershipText.toLowerCase());
+
+        const imageSrc = biz.image && biz.image.trim() !== "" ? biz.image : "images/placeholder.png";
+
+        card.innerHTML = `
+      <img src="${imageSrc}" alt="${biz.name}" class="directory-img">
+      <h3>${biz.name}</h3>
+      <p>${biz.address}</p>
+      <p>${biz.phone}</p>
+
+      <p class="membership-tag">Membership: ${membershipText}</p>
+
+      <a href="${biz.website}" target="_blank" class="cta-btn">Visit Website</a>
+    `;
+
+        directoryContainer.appendChild(card);
+    });
+
+    directoryContainer.classList.add('grid');
+    directoryContainer.classList.remove('list');
+    gridBtn.classList.add('active');
+    listBtn.classList.remove('active');
+
+}
+
+function displayList() {
+    directoryContainer.innerHTML = '';
+
+    businesses.forEach(biz => {
+        const row = document.createElement('section');
+        row.classList.add('list-item');
+
+        row.innerHTML = `
+      <h3>${biz.name}</h3>
+      <p>${biz.phone}</p>
+      <a href="${biz.website}" target="_blank">Visit Website</a>
+    `;
+
+        directoryContainer.appendChild(row);
+    });
+
+    directoryContainer.classList.add('list');
+    directoryContainer.classList.remove('grid');
+    listBtn.classList.add('active');
+    gridBtn.classList.remove('active');
+}
+
+gridBtn.addEventListener('click', displayGrid);
+listBtn.addEventListener('click', displayList);
